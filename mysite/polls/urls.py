c9a337a44f325eda15781b281afa7c51e12bad06
Mo Leigh <17851454+MoLeighJackson@users.noreply.github.com>
2021-06-09 08:43:21 +0000

    writing more views

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,6 +2,15 @@ from django.urls import path
 
 from . import views
 
+#wire poll/views.py views to urls
 urlpatterns = [
-    path('', views.index, name='index')
-    ]
+
+    # polls view is index view
+    path('', views.index, name='index'),
+    # polls/question id num
+    path('<int:question_id>/', views.detail, name='detail'),
+    # polls/question id num/results
+    path('<int:question_id>/results/', views.results, name='results'),
+    # polls/question id num/vote
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -6,3 +6,13 @@ def index(request):
 
 def home(request):
     return HttpResponse("Home Page")
+
+def detail(request, question_id):
+    return HttpResponse("This is question %s." % question_id)
+
+def results(request, question_id):
+    response = "The results for question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Voting on question %s." % question_id)
