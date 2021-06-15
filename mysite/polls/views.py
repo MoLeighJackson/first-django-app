4fa0f3583195d71af117da877df52c5a9b7f2a23
Mo Leigh <17851454+MoLeighJackson@users.noreply.github.com>
2021-06-15 21:01:04 +0000

    writing views that actually do something

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,17 @@
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Polls Index")
+    #display latest 10 polls questions on the index page
+    latest_question_list = Question.objects.order_by('-pub_date')[:10]
+    #separate poll questions with *
+    template = loader.get_template('polls/index.html')
+    context = {'latest_question_list': latest_question_list,
+            }
+    return HttpResponse(template.render(context, request))
 
 def home(request):
     return HttpResponse("Home Page")
@@ -16,3 +25,4 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("Voting on question %s." % question_id)
+
