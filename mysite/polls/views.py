5cc1f1ec1b561a76921eb8a2fcd5c80329c9987a
Mo Leigh <17851454+MoLeighJackson@users.noreply.github.com>
2021-06-17 03:08:56 +0000

    raising a 404 error

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Question
 
@@ -13,7 +13,8 @@ def home(request):
     return HttpResponse("Home Page")
 
 def detail(request, question_id):
-    return HttpResponse("This is question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "The results for question %s."
