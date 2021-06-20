f47805196f49fe5109d60a3100afc3ce9c340aad
Mo Leigh <17851454+MoLeighJackson@users.noreply.github.com>
2021-06-20 18:44:03 +0000

    Use generic views: Less code is better

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -5,13 +5,8 @@ from . import views
 app_name = 'polls'
 #wire poll/views.py views to urls
 urlpatterns = [
-
-    # polls view is index view
-    path('', views.index, name='index'),
-    # polls/question id num
-    path('<int:question_id>/', views.detail, name='detail'),
-    # polls/question id num/results
-    path('<int:question_id>/results/', views.results, name='results'),
-    # polls/question id num/vote
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
