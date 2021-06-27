bb094ff8546985af9bc2cd6f28c38dd7349cb081
Mo Leigh <17851454+MoLeighJackson@users.noreply.github.com>
2021-06-27 23:09:45 +0000

    initial commit for wildflower site

---
+++ mysite/adoptions/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+#defines the expected structure of the database
+
+#model for storing pet info
+class Pet(models.Model):
+    SEX_CHOICES = [('M', 'Male'), ('F', 'Female')]
+    name = models.CharField(max_length=100)
+    submitter = models.CharField(max_length=100)
+    species = models.CharField(max_length=30)
+    breed = models.CharField(max_length=30, blank=True)
+    description = models.TextField()
+    sex = models.CharField(max_length=1, choices=SEX_CHOICES, blank=True)
+    submission_date = models.DateTimeField()
+    age = models.IntegerField(null=True)
+    vaccinations = models.ManyToManyField('Vaccine', blank=True)
+
+#model for tracking vaccines
+class Vaccine(models.Model):
+    name = models.CharField(max_length=50)
